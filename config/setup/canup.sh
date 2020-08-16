0f6dbf92c098d204e5ce6d76ed24575549d09645
Owen <ohowland@gmail.com>
2020-08-16 11:56:56 -0600

    fix bug in canup.sh path

--- config/setup/canup.sh
+++ config/setup/canup.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 
-echo "auto can0\niface can0 inet manual\n  pre-up /sbin/ip link set $IFACE type can bitrate 500000\n up /sbin/ifconfig $IFACE up\n down /sbinifconfig $IFACE down" > ~/tmp.txt
+CAN_INTERFACE="/etc/network/interfaces"
+BAUDRATE=500000
+
+echo -e "auto can0\niface can0 inet manual\n  pre-up /sbin/ip link set \$IFACE type can bitrate $BAUDRATE\n  up /sbin/ifconfig \$IFACE up\n  down /sbin/ifconfig \$IFACE down\n" >> $CAN_INTERFACE 
+
+echo -e "auto can1\niface can1 inet manual\n  pre-up /sbin/ip link set \$IFACE type can bitrate $BAUDRATE\n  up /sbin/ifconfig \$IFACE up\n  down /sbin/ifconfig \$IFACE down" >> $CAN_INTERFACE
+
+/etc/init.d/network-manager restart
